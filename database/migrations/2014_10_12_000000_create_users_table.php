999b6ea0f6eb0f7801759bf0599ad5c82f5b97e0
Tiago Alves <tiagooliveiraaso2@gmail.com>
2022-08-24 15:48:54 -0300

    ajustes migration user add logo

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name',255);
+            $table->string('logo',255)->nullable();
             $table->string('email')->unique();
             $table->string('login')->unique();
             $table->enum('tipo', ['dev_admin', 'dev_empregado','user_admin','user_empregado']);
