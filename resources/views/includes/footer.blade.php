ab961db703977f34f4a7547388e98479605284fa
Tiago Alves <tiagooliveiraaso2@gmail.com>
2022-08-25 15:08:53 -0300

    ajustes divisao footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,1 +1,26 @@
-<script src="{{asset('js/footer.js')}}"></script>
+{{-- meu script --}}
+@if (session()->has('alert'))
+  @php
+    $data = htmlspecialchars_decode(session('alert.data'));
+  @endphp
+  <script>
+    showAlert("{{session('alert.titulo')}}", "<?= $data ?>", "{{session('alert.tipo')}}")
+  </script>
+  @php
+    session()->forget('alert');
+  @endphp
+@endif
+<!-- plugins:js -->
+<script src="{{ asset('vendors/js/vendor.bundle.base.js') }}"></script>
+<!-- endinject -->
+<!-- Plugin js for this page -->
+<!-- End plugin js for this page -->
+<!-- inject:js -->
+<script src="{{ asset('js/plugins/off-canvas.js') }}"></script>
+<script src="{{ asset('js/plugins/hoverable-collapse.js') }}"></script>
+<script src="{{ asset('js/plugins/misc.js') }}"></script>
+<script src="{{ asset('js/plugins/settings.js') }}"></script>
+<script src="{{ asset('js/plugins/todolist.js') }}"></script>
+<!-- endinject -->
+<!-- Custom js for this page -->
+<!-- End custom js for this page -->
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -84,30 +84,7 @@
       </div>
       <!-- page-body-wrapper ends -->
     </div>
-    <!-- container-scroller -->
-    <!-- plugins:js -->
-    <script src="{{ asset('vendors/js/vendor.bundle.base.js"') }}"></script>
-    <!-- endinject -->
-    <!-- Plugin js for this page -->
-    <!-- End plugin js for this page -->
-    <!-- inject:js -->
-    <script src="{{ asset('js/plugins/off-canvas.js') }}"></script>
-    <script src="{{ asset('js/plugins/hoverable-collapse.js') }}"></script>
-    <script src="{{ asset('js/plugins/misc.js') }}"></script>
-    <script src="{{ asset('js/plugins/settings.js') }}"></script>
-    <script src="{{ asset('js/plugins/todolist.js') }}"></script>
-    <!-- endinject -->
-
-    @if (session()->has('alert'))
-      @php
-        $data = htmlspecialchars_decode(session('alert.data'));
-      @endphp
-      <script>
-        showAlert("{{session('alert.titulo')}}", "<?= $data ?>", "{{session('alert.tipo')}}")
-      </script>
-      @php
-        session()->forget('alert');
-      @endphp
-    @endif
+    @include('includes.footer');
+    
   </body>
 </html>
