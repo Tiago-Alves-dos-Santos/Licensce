7d39ad0fdf4d12638d1e4f8dfb5c12efa6ba0584
Tiago Alves <tiagooliveiraaso2@gmail.com>
2022-08-25 15:24:00 -0300

    sistema divido em layout

--- app/Http/Controllers/devAdmin/Admin.php
+++ app/Http/Controllers/devAdmin/Admin.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers\devAdmin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use PhpParser\Node\Expr\Cast\Object_;
 
 class Admin extends Controller
 {
     public function index(Request $request)
     {
-        return view('dev-admin.index');
+        $page_data = [
+            'menu' => 'inicio',
+            'titulo' => 'Início',
+            'breadcumb' => ['Areá Principal']
+        ];
+        return view('dev-admin.index', [
+            'page_data' => (object) $page_data
+        ]);
     }
 }
--- resources/views/dev-admin/index.blade.php
+++ resources/views/dev-admin/index.blade.php
@@ -1,42 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-  @include('includes.header')
-  <body>
-    <div class="container-scroller">
-      <!-- partial:../../partials/_sidebar.html -->
-      @include('includes.sidebar')
-      <!-- partial -->
-      <div class="container-fluid page-body-wrapper">
-        <!-- partial:../../partials/_navbar.html -->
-        @include('includes.navbar')
-        <!-- partial -->
-        <div class="main-panel">
-          <div class="content-wrapper">
-            <div class="page-header">
-                <h3 class="page-title"> Form elements </h3>
-                <nav aria-label="breadcrumb">
-                  <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="#">Forms</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Form elements</li>
-                  </ol>
-                </nav>
-              </div>
-          </div>
-          <!-- content-wrapper ends -->
-          <!-- partial:../../partials/_footer.html -->
-          <footer class="footer">
-            <div class="d-sm-flex justify-content-center justify-content-sm-between">
-              <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
-              <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Free <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">Bootstrap admin templates</a> from Bootstrapdash.com</span>
-            </div>
-          </footer>
-          <!-- partial -->
-        </div>
-        <!-- main-panel ends -->
-      </div>
-      <!-- page-body-wrapper ends -->
-    </div>
-    <!-- container-scroller -->
-    @include('includes.footer')
-  </body>
-</html>
+@extends('layouts.painel-admin')
+
+@section('conteudo')
+<h1>Dasboard</h1>
+@endsection
